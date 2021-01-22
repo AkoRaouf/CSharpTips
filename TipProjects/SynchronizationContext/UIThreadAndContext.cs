56ce0924978d7638336ec9adcd8f96871b14b9de
Raouf <acer@DESKTOP-HMQQE1N>
2021-01-22 16:05:31 +0330

    Use the sc directly

--- TipProjects/SynchronizationContext/UIThreadAndContext.cs
+++ TipProjects/SynchronizationContext/UIThreadAndContext.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace CShapAdvancedTips.SynchronizationContext
+namespace CShapAdvancedTips.Context
 {
     public class UIThreadAndContext
     {
@@ -19,6 +20,21 @@ namespace CShapAdvancedTips.SynchronizationContext
                 {
                     btn.Contect = downlaodTask.Result;
                 }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public void DownloadAndUpdateTheButtonTextDirectSc()
+        {
+            SynchronizationContext sc = SynchronizationContext.Current;
+            ///This is when for example you want to update the contect of 
+            ///a button in the UI thered so you have to do like this;
+            _httpClient.GetStringAsync("http://time.com/currentTime")
+                .ContinueWith(downlaodTask =>
+                {
+                    sc.Post(delegate
+                    {
+                        btn.Contect = downlaodTask.Result;
+                    }, null);
+                });
         }
     }
 }
